c3def1f447901eca307163b7479b048e26e83197
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-16 14:21:56 +0700

    test: add test at not found error creation

---
+++ src/commons/exceptions/_test/not-found-error.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { NotFoundError } from '../not-found-error';
+
+describe('NotFoundError', () => {
+  it('should create an instance of NotFoundError', () => {
+    const notFoundError = new NotFoundError('NOT_FOUND_ERROR');
+
+    expect(notFoundError.statusCode).toEqual(404);
+    expect(notFoundError.message).toEqual('NOT_FOUND_ERROR');
+    expect(notFoundError.name).toEqual('NotFoundError');
+  });
+});
