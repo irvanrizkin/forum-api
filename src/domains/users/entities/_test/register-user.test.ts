5dde147f51fbbf46b10ff4d952de9c998eb49af0
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-17 06:59:46 +0700

    test: add test for register user entity

---
+++ src/domains/users/entities/_test/register-user.test.ts
@@ -0,0 +1,76 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { RegisterUser } from '../register-user';
+
+describe('a RegisterUser entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      username: 'john',
+      password: 'secret',
+    };
+
+    // Action & Assert
+    expect(() => new RegisterUser(payload)).toThrowError(
+      'REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY',
+    );
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      username: 123,
+      fullname: true,
+      password: 'abc',
+    };
+
+    // Action & Assert
+    expect(() => new RegisterUser(payload)).toThrowError(
+      'REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION',
+    );
+  });
+
+  it('should throw error when username contains more than 50 character', () => {
+    // Arrange
+    const payload = {
+      username: 'john'.repeat(15),
+      fullname: 'John Doe',
+      password: 'secret',
+    };
+
+    // Action & Assert
+    expect(() => new RegisterUser(payload)).toThrowError(
+      'REGISTER_USER.USERNAME_LIMIT_CHAR',
+    );
+  });
+
+  it('should throw error when username contains restricted character', () => {
+    // Arrange
+    const payload = {
+      username: 'john doe',
+      fullname: 'John Doe',
+      password: 'secret',
+    };
+
+    // Action & Assert
+    expect(() => new RegisterUser(payload)).toThrowError(
+      'REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER',
+    );
+  });
+
+  it('should create registerUser object correctly', () => {
+    const payload = {
+      username: 'john',
+      fullname: 'John Doe',
+      password: 'secret',
+    };
+
+    // Action
+    const { username, fullname, password } = new RegisterUser(payload);
+
+    // Assert
+    expect(username).toEqual(payload.username);
+    expect(fullname).toEqual(payload.fullname);
+    expect(password).toEqual(payload.password);
+  });
+});
---
+++ src/domains/users/entities/register-user.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+class RegisterUser {}
+
+export { RegisterUser };
