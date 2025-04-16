09e513f6e31ed5cd6294a5114437c2569b58cae1
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-16 13:26:57 +0700

    test: add test at invariant error creation

---
+++ src/commons/exceptions/_test/invariant-error.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { InvariantError } from '../invariant-error';
+
+describe('InvariantError', () => {
+  it('should create an error correctly', () => {
+    const invariantError = new InvariantError('INVARIANT_ERROR');
+
+    expect(invariantError.statusCode).toEqual(400);
+    expect(invariantError.message).toEqual('INVARIANT_ERROR');
+    expect(invariantError.name).toEqual('InvariantError');
+  });
+});
