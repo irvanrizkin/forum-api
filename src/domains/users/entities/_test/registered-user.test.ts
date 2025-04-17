f86a13297dfe951daf22c7b7dcd672a0efe0ffe9
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-17 18:19:27 +0700

    test: add test for registered user entity

---
+++ src/domains/users/entities/_test/registered-user.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { RegisteredUser } from '../registered-user';
+
+describe('a RegisteredUser entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      username: 'john',
+      fullname: 'John Doe',
+    };
+
+    // Action & Assert
+    expect(() => new RegisteredUser(payload)).toThrowError(
+      'REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY',
+    );
+  });
+
+  it('should create registeredUser object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'user-016',
+      username: 'john',
+      fullname: 'John Doe',
+    };
+
+    // Action
+    const { id, username, fullname } = new RegisteredUser(payload);
+
+    // Assert
+    expect(id).toEqual(payload.id);
+    expect(username).toEqual(payload.username);
+    expect(fullname).toEqual(payload.fullname);
+  });
+});
