35bc8b1ab74422db55710a3bbdb8f6ed9f3e4a17
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-17 18:26:07 +0700

    feat: add registered user entity

--- src/domains/users/entities/registered-user.ts
+++ src/domains/users/entities/registered-user.ts
@@ -1,4 +1,33 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-class RegisteredUser {}
+interface RegisteredUserPayload {
+  id?: string;
+  username?: string;
+  fullname?: string;
+}
+
+class RegisteredUser {
+  id: string;
+  username: string;
+  fullname: string;
+
+  constructor(payload: RegisteredUserPayload) {
+    this.verifyPayload(payload);
+
+    const { id, username, fullname } = payload;
+
+    this.id = id;
+    this.username = username;
+    this.fullname = fullname;
+  }
+
+  private verifyPayload(
+    payload: RegisteredUserPayload,
+  ): asserts payload is Required<RegisteredUserPayload> {
+    const { id, username, fullname } = payload;
+
+    if (!id || !username || !fullname) {
+      throw new Error('REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
+}
 
 export { RegisteredUser };
