9c4c0060619c7bba3df3d79bb6300114ad101731
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-17 08:21:10 +0700

    feat: add register user entity

--- src/domains/users/entities/register-user.ts
+++ src/domains/users/entities/register-user.ts
@@ -1,4 +1,47 @@
-/* eslint-disable @typescript-eslint/no-extraneous-class */
-class RegisterUser {}
+interface RegisterUserPayload {
+  username?: string;
+  fullname?: string;
+  password?: string;
+}
+
+class RegisterUser {
+  username: string;
+  fullname: string;
+  password: string;
+
+  constructor(payload: RegisterUserPayload) {
+    this.verifyPayload(payload);
+
+    const { username, fullname, password } = payload;
+
+    this.username = username ?? '';
+    this.fullname = fullname ?? '';
+    this.password = password ?? '';
+  }
+
+  private verifyPayload(payload: RegisterUserPayload) {
+    const { username, fullname, password } = payload;
+
+    if (!username || !fullname || !password) {
+      throw new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof username !== 'string' ||
+      typeof fullname !== 'string' ||
+      typeof password !== 'string'
+    ) {
+      throw new TypeError('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+
+    if (username.length > 50) {
+      throw new Error('REGISTER_USER.USERNAME_LIMIT_CHAR');
+    }
+
+    if (/^[\w]+$/.test(username)) {
+      throw new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
+    }
+  }
+}
 
 export { RegisterUser };
