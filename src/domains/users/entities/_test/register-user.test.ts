8684c99f2bd432f439902985e01decec142e278b
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-17 09:23:33 +0700

    test: remove type test due to typescript

--- src/domains/users/entities/_test/register-user.test.ts
+++ src/domains/users/entities/_test/register-user.test.ts
@@ -16,20 +16,6 @@ describe('a RegisterUser entities', () => {
     );
   });
 
-  it('should throw error when payload did not meet data type specification', () => {
-    // Arrange
-    const payload = {
-      username: 123,
-      fullname: true,
-      password: 'abc',
-    };
-
-    // Action & Assert
-    expect(() => new RegisterUser(payload)).toThrowError(
-      'REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION',
-    );
-  });
-
   it('should throw error when username contains more than 50 character', () => {
     // Arrange
     const payload = {
--- src/domains/users/entities/register-user.ts
+++ src/domains/users/entities/register-user.ts
@@ -14,31 +14,25 @@ class RegisterUser {
 
     const { username, fullname, password } = payload;
 
-    this.username = username ?? '';
-    this.fullname = fullname ?? '';
-    this.password = password ?? '';
+    this.username = username;
+    this.fullname = fullname;
+    this.password = password;
   }
 
-  private verifyPayload(payload: RegisterUserPayload) {
+  private verifyPayload(
+    payload: RegisterUserPayload,
+  ): asserts payload is Required<RegisterUserPayload> {
     const { username, fullname, password } = payload;
 
     if (!username || !fullname || !password) {
       throw new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
-    if (
-      typeof username !== 'string' ||
-      typeof fullname !== 'string' ||
-      typeof password !== 'string'
-    ) {
-      throw new TypeError('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
-    }
-
     if (username.length > 50) {
       throw new Error('REGISTER_USER.USERNAME_LIMIT_CHAR');
     }
 
-    if (/^[\w]+$/.test(username)) {
+    if (!/^[\w]+$/.test(username)) {
       throw new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
     }
   }
