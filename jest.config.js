7f65c7a8d917d773562eceb38e14ff518a105b8e
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-18 10:55:23 +0700

    feat: implement add user use case and jest alias mapper

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ export default {
   transform: {
     '^.+\.tsx?$': ['ts-jest', {}],
   },
+  moduleNameMapper: {
+    '^@/(.*)': '<rootDir>/src/$1',
+  },
 };
--- src/applications/use_case/add-user-use-case.ts
+++ src/applications/use_case/add-user-use-case.ts
@@ -1,4 +1,37 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-class AddUserUseCase {}
+import { UserRepository } from '@/domains/users/user-repository';
+
+import { PasswordHash } from '../security/password-hash';
+
+interface AddUserUseCasePayload {
+  username: string;
+  password: string;
+  fullname: string;
+}
+
+interface AddUserUseCaseDependencies {
+  userRepository: UserRepository;
+  passwordHash: PasswordHash;
+}
+
+class AddUserUseCase {
+  private userRepository: UserRepository;
+  private passwordHash: PasswordHash;
+  constructor({ userRepository, passwordHash }: AddUserUseCaseDependencies) {
+    this.userRepository = userRepository;
+    this.passwordHash = passwordHash;
+  }
+
+  async execute(useCasePayload: AddUserUseCasePayload) {
+    const { username, password, fullname } = useCasePayload;
+
+    await this.userRepository.verifyAvailableUsername(username);
+    const hashedPassword = await this.passwordHash.hash(password);
+    return this.userRepository.addUser({
+      username,
+      password: hashedPassword,
+      fullname,
+    });
+  }
+}
 
 export { AddUserUseCase };
