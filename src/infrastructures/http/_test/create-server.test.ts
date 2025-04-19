27a69ad8b2451bc3d2d66ee80a6293339fa52be3
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-04-19 10:26:09 +0700

    test: add test for create server

---
+++ src/infrastructures/http/_test/create-server.test.ts
@@ -0,0 +1,43 @@
+import { afterAll, afterEach, describe } from '@jest/globals';
+
+import { pool } from '@/infrastructures/database/postgres/pool';
+import { createServer } from '@/infrastructures/http/create-server';
+
+import { UsersTableTestHelper } from '@/tests/users-table-test-helper';
+
+describe('HTTP server', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /users', () => {
+    it('should response 201 and persisted user', async () => {
+      // Arrange
+      const server = await createServer();
+      const requestPayload = {
+        username: 'john',
+        password: 'secret',
+        fullname: 'John Doe',
+      };
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedUser.username).toEqual(
+        requestPayload.username,
+      );
+    });
+  });
+});
---
+++ src/infrastructures/http/create-server.ts
@@ -0,0 +1,3 @@
+const createServer = async () => {};
+
+export { createServer };
